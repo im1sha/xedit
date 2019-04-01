d5a2e30615c8e5743bc67b6dc66b16840fe0f781
im1sha <vkporsche@ya.ru>
2019-04-01 13:33:49 +0300

    image opening was embedded to master page structure

--- XEdit/XEdit/XEdit/App.cs
+++ XEdit/XEdit/XEdit/App.cs
@@ -9,51 +9,7 @@ namespace XEdit
 	{
 		public App ()
 		{
-			//MainPage = new XEdit.XAML.MainPage ();
-
-            var stack = new StackLayout();
-            MainPage = new ContentPage
-            {
-                Content = stack
-            };
-
-            // Picture Picker
-            Button pickPictureButton = new Button
-            {
-                Text = "Pick Photo",
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                HorizontalOptions = LayoutOptions.CenterAndExpand
-            };
-            stack.Children.Add(pickPictureButton);
-
-            pickPictureButton.Clicked += async (sender, e) =>
-            {
-                pickPictureButton.IsEnabled = false;
-                Stream stream = await DependencyService.Get<PicturePicker.IPicturePicker>().GetImageStreamAsync();
-
-                if (stream != null)
-                {
-                    Image image = new Image
-                    {
-                        Source = ImageSource.FromStream(() => stream),
-                        BackgroundColor = Color.Gray
-                    };
-
-                    TapGestureRecognizer recognizer = new TapGestureRecognizer();
-                    recognizer.Tapped += (sender2, args) =>
-                    {
-                        (MainPage as ContentPage).Content = stack;
-                        pickPictureButton.IsEnabled = true;
-                    };
-                    image.GestureRecognizers.Add(recognizer);
-
-                    (MainPage as ContentPage).Content = image;
-                }
-                else
-                {
-                    pickPictureButton.IsEnabled = true;
-                }
-            };
+			MainPage = new XEdit.XAML.MainPage ();
         }
 
 		protected override void OnStart ()
--- XEdit/XEdit/XEdit/XAML/GalleryPage.xaml.cs
+++ XEdit/XEdit/XEdit/XAML/GalleryPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,48 @@ using Xamarin.Forms.Xaml;
 namespace XEdit.XAML
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class PhotoListPage : ContentPage
+	public partial class GalleryPage : ContentPage
 	{
-		public PhotoListPage ()
+		public GalleryPage()
 		{
 			InitializeComponent ();
 
+            Button pickPictureButton = new Button
+            {
+                Text = "Pick Photo",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand
+            };
+            stack.Children.Add(pickPictureButton);
 
-		}
+            pickPictureButton.Clicked += async (sender, e) =>
+            {
+                pickPictureButton.IsEnabled = false;
+                Stream stream = await DependencyService.Get<PicturePicker.IPicturePicker>().GetImageStreamAsync();
+
+                if (stream != null)
+                {
+                    Image image = new Image
+                    {
+                        Source = ImageSource.FromStream(() => stream),
+                        BackgroundColor = Color.Gray
+                    };
+
+                    TapGestureRecognizer recognizer = new TapGestureRecognizer();
+                    recognizer.Tapped += (sender2, args) =>
+                    {
+                        (this as ContentPage).Content = stack;
+                        pickPictureButton.IsEnabled = true;
+                    };
+                    image.GestureRecognizers.Add(recognizer);
+
+                    (this as ContentPage).Content = image;
+                }
+                else
+                {
+                    pickPictureButton.IsEnabled = true;
+                }
+            };
+        }
 	}
 }
