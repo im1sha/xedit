5157889573b8d65364df2249c0343920362e6649
im1sha <vkporsche@ya.ru>
2019-04-02 20:31:47 +0300

    SkiaBase added

---
+++ XEdit/XEdit/XEdit/Skia/Transforms/TouchManipulationMode.cs
@@ -0,0 +1,12 @@
+﻿namespace SkiaBase.Transforms
+{
+    enum TouchManipulationMode
+    {
+        None,
+        PanOnly,
+        IsotropicScale,     // includes panning
+        AnisotropicScale,   // includes panning
+        ScaleRotate,        // implies isotropic scaling
+        ScaleDualRotate     // adds one-finger rotation
+    }
+}
